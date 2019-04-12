2f32efa2545fcc4c4e890bad67730ef0d1d07cde
Victoria Chhieng <victoria_chhieng@hotmail.com>
2019-04-12 13:24:44 -0500

    different shapes displaying on dom

--- index.vr.js
+++ index.vr.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, AppRegistry, StyleSheet } from 'react-vr';
-import Shape from './vr/components/Shape';
+import Shape, { shapes } from './vr/components/Shape';
 
 class ShapeGame extends Component {
   constructor() {
@@ -11,11 +11,32 @@ class ShapeGame extends Component {
     }
   }
 
+  componentDidMount() {
+    this.newGameSet();
+  }
+
+  newGameSet() {
+    let baseShapeId = Math.floor(Math.random() * shapes.length);
+
+    let specialShapeId = baseShapeId;
+    while (specialShapeId === baseShapeId) {
+      specialShapeId = Math.floor(Math.random() * shapes.length);
+    }
+
+    let newGameShapes = [];
+    for (let i = 0; i < this.state.gameShapes.length; i++)
+      newGameShapes[i] = baseShapeId;
+
+    let specialIndex = Math.floor(Math.random() * newGameShapes.length);
+    newGameShapes[specialIndex] = specialIndex;
+
+    this.setState({ gameShapes: newGameShapes })
+  }
+
   render() {
     return (
       <View style={styles.game}>
         <Text style={styles.text}>Find the Odd Shape!</Text>
-        {/* <Shape shapeNum={0} transform={[{translate: [0, 0, -5]}]}/> */}
         {
           this.state.gameShapes.map((shape, index) => {
             return (
@@ -23,7 +44,7 @@ class ShapeGame extends Component {
                 <Shape
                   shapeNum={shape}
                   colorNum={index}
-                  transform={[{translate: [(index-1.5)*1.5, 0, -5]}]}
+                  transform={[{ translate: [(index - 1.5) * 1.5, 0, -5] }]}
                 />
               </View>
             )
@@ -37,7 +58,7 @@ class ShapeGame extends Component {
 const styles = StyleSheet.create({
   game: {
     transform: [
-      {translate: [-2.25, 0, 0]}
+      { translate: [-2.25, 0, 0] }
     ]
   },
   text: {
--- vr/components/Shape.js
+++ vr/components/Shape.js
@@ -3,6 +3,8 @@ import { Box, Sphere, Cylinder } from 'react-vr';
 
 const shapes = [Box, Sphere, Cylinder];
 
+export { shapes };
+
 
 export default class Shape extends Component {
     render() {
